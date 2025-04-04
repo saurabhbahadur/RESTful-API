715e8c2d3ee125acc66e5b83f79e556cd0302102
Saurabh Bahadur <133571086+saurabhbahadur@users.noreply.github.com>
2025-04-04 23:07:33 +0530

    spring and express backend coming soon....

---
+++ backend-springBoot/src/main/java/mighty/backend_springBoot/BackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package mighty.backend_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ backend-springBoot/src/test/java/mighty/backend_springBoot/BackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mighty.backend_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
